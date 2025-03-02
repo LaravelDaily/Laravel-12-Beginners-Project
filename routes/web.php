4bc0f199f99b407c2b17f65737af9ee1f970a750
PovilasKorop <povilask007@gmail.com>
2025-03-02 12:10:16 +0200

    Route names

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-Route::view('contact', 'contact');
-Route::view('about', 'about');
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
+Route::view('about', 'about')->name('about');
 
 Route::view('/second', 'second');
