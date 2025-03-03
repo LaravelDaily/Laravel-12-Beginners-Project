5f51c127b96f5773d631445768ce0b8a0951ff7b
PovilasKorop <povilask007@gmail.com>
2025-03-03 07:31:20 +0200

    New layout for the blog

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
+Route::view('article', 'article')->name('article');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 
