4d95f5abc22284819e0e9e7fcf8e762072ff0fe6
PovilasKorop <povilask007@gmail.com>
2025-03-03 10:05:49 +0200

    Data from DB in Route callback function

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::view('/', 'home')->name('home');
+Route::get('/', function () {
+    $categories = DB::table('categories')->get();
+    return view('home', ['categories' => $categories]);
+})->name('home');
+
 Route::view('article', 'article')->name('article');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
